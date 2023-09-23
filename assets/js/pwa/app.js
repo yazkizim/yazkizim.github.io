ee2c3be79bb7b18dba3b4fc48b3216c7f604afd7
yazkizim <100561777+yazkizim@users.noreply.github.com>
2023-09-24 02:19:55 +0300

    yükle

--- assets/js/pwa/app.js
+++ assets/js/pwa/app.js
@@ -57,7 +57,9 @@ $.ajax({
 		} else if (data['USD']['d_oran'] < 0) {
 			$('#dolar').attr('class', 'text-danger');
 			$('#dolar').html(parseFloat(data['USD']['satis']).toFixed(2) + ' ₺');
-		}
+		} else {
+            $('#dolar').html(parseFloat(data['USD']['satis']).toFixed(2) + ' ₺');
+        }
 
 		if (data['EUR']['d_oran'] > 0) { 
 			$('#euro').attr('class', 'text-success');
@@ -65,7 +67,9 @@ $.ajax({
 		} else if (data['EUR']['d_oran'] < 0){
 			$('#euro').attr('class', 'text-danger');
 			$('#euro').html(parseFloat(data['EUR']['satis']).toFixed(2) + ' ₺');
-		}
+		} else {
+            $('#euro').html(parseFloat(data['EUR']['satis']).toFixed(2) + ' ₺');
+        }
 
 		if (data['GA']['d_oran'] > 0) { 
 			$('#altin').attr('class', 'text-success');
@@ -73,7 +77,9 @@ $.ajax({
 		} else if (data['GA']['d_oran'] < 0) {
 			$('#altin').attr('class', 'text-danger');
 			$('#altin').html(parseFloat(data['GA']['satis']).toFixed(2) + ' ₺');
-		}
+		} else {
+            $('#altin').html(parseFloat(data['GA']['satis']).toFixed(2) + ' ₺');
+        }
 
 		if (data['XU100']['degisim'] > 0) { 
 			$('#bist').attr('class', 'text-success');
@@ -81,7 +87,9 @@ $.ajax({
 		} else if (data['XU100']['degisim'] < 0) {
 			$('#bist').attr('class', 'text-danger');
 			$('#bist').html(parseFloat(data['XU100']['satis']).toFixed(2) + ' ₺');
-		}
+		} else {
+            $('#bist').html(parseFloat(data['XU100']['satis']).toFixed(2) + ' ₺');
+        }
 
 		if (data['BTC']['d_oran'] > 0) { 
 			$('#btc').attr('class', 'text-success');
@@ -89,7 +97,9 @@ $.ajax({
 		} else if (data['BTC']['d_oran'] < 0){
 			$('#btc').attr('class', 'text-danger');
 			$('#btc').html(parseFloat(data['BTC']['satis']).toFixed(2) + ' ₺');
-		}
+		} else {
+            $('#btc').html(parseFloat(data['BTC']['satis']).toFixed(2) + ' ₺');
+        }
 	},
 	error: function(){
 		$('#dovizbar').hide();
