8ded803cf896d053536ecf1a0c6e848c872a97e3
yazkizim <100561777+yazkizim@users.noreply.github.com>
2023-09-24 21:15:17 +0300

    yükle

--- assets/js/pwa/app.js
+++ assets/js/pwa/app.js
@@ -53,52 +53,52 @@ $.ajax({
 	success: function(data) {
 		if (data['USD']['d_oran'] > 0) { 
 			$('#dolar').attr('class', 'text-success');
-			$('#dolar').html(parseFloat(data['USD']['satis']).toFixed(2) + '₺');
+			$('#dolar').html("<br>" + parseFloat(data['USD']['satis']).toFixed(2) + ' ₺');
 		} else if (data['USD']['d_oran'] < 0) {
 			$('#dolar').attr('class', 'text-danger');
-			$('#dolar').html(parseFloat(data['USD']['satis']).toFixed(2) + '₺');
+			$('#dolar').html("<br>" + parseFloat(data['USD']['satis']).toFixed(2) + ' ₺');
 		} else {
-            $('#dolar').html(parseFloat(data['USD']['satis']).toFixed(2) + '₺');
+            $('#dolar').html("<br>" + parseFloat(data['USD']['satis']).toFixed(2) + ' ₺');
         }
 
 		if (data['EUR']['d_oran'] > 0) { 
 			$('#euro').attr('class', 'text-success');
-			$('#euro').html(parseFloat(data['EUR']['satis']).toFixed(2) + '₺');
+			$('#euro').html("<br>" + parseFloat(data['EUR']['satis']).toFixed(2) + ' ₺');
 		} else if (data['EUR']['d_oran'] < 0){
 			$('#euro').attr('class', 'text-danger');
-			$('#euro').html(parseFloat(data['EUR']['satis']).toFixed(2) + '₺');
+			$('#euro').html("<br>" + parseFloat(data['EUR']['satis']).toFixed(2) + ' ₺');
 		} else {
-            $('#euro').html(parseFloat(data['EUR']['satis']).toFixed(2) + '₺');
+            $('#euro').html("<br>" + parseFloat(data['EUR']['satis']).toFixed(2) + ' ₺');
         }
 
 		if (data['GA']['d_oran'] > 0) { 
 			$('#altin').attr('class', 'text-success');
-			$('#altin').html(parseFloat(data['GA']['satis']).toFixed(2) + '₺');
+			$('#altin').html("<br>" + parseFloat(data['GA']['satis']).toFixed(2) + ' ₺');
 		} else if (data['GA']['d_oran'] < 0) {
 			$('#altin').attr('class', 'text-danger');
-			$('#altin').html(parseFloat(data['GA']['satis']).toFixed(2) + '₺');
+			$('#altin').html("<br>" + parseFloat(data['GA']['satis']).toFixed(2) + ' ₺');
 		} else {
-            $('#altin').html(parseFloat(data['GA']['satis']).toFixed(2) + '₺');
+            $('#altin').html("<br>" + parseFloat(data['GA']['satis']).toFixed(2) + ' ₺');
         }
 
 		if (data['XU100']['degisim'] > 0) { 
 			$('#bist').attr('class', 'text-success');
-			$('#bist').html(parseFloat(data['XU100']['satis']).toFixed(2) + '₺');
+			$('#bist').html("<br>" + parseFloat(data['XU100']['satis']).toFixed(2) + ' ₺');
 		} else if (data['XU100']['degisim'] < 0) {
 			$('#bist').attr('class', 'text-danger');
-			$('#bist').html(parseFloat(data['XU100']['satis']).toFixed(2) + '₺');
+			$('#bist').html("<br>" + parseFloat(data['XU100']['satis']).toFixed(2) + ' ₺');
 		} else {
-            $('#bist').html(parseFloat(data['XU100']['satis']).toFixed(2) + '₺');
+            $('#bist').html("<br>" + parseFloat(data['XU100']['satis']).toFixed(2) + ' ₺');
         }
 
 		if (data['BTC']['d_oran'] > 0) { 
 			$('#btc').attr('class', 'text-success');
-			$('#btc').html(parseFloat(data['BTC']['satis']).toFixed(2) + '$');
+			$('#btc').html("<br>" + parseFloat(data['BTC']['satis']).toFixed(2) + ' $');
 		} else if (data['BTC']['d_oran'] < 0){
 			$('#btc').attr('class', 'text-danger');
-			$('#btc').html(parseFloat(data['BTC']['satis']).toFixed(2) + '$');
+			$('#btc').html("<br>" + parseFloat(data['BTC']['satis']).toFixed(2) + ' $');
 		} else {
-            $('#btc').html(parseFloat(data['BTC']['satis']).toFixed(2) + '$');
+            $('#btc').html("<br>" + parseFloat(data['BTC']['satis']).toFixed(2) + ' $');
         }
 	},
 	error: function(){
